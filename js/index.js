3f3479f0344db0524f02e80bf476fdc1175f959a
Abass <Abassolanrewaju111@gmail.com>
2022-01-15 21:36:32 +0100

    complete mobile menu

--- js/index.js
+++ js/index.js
@@ -2,13 +2,15 @@ const display = () => {
   const hamburger = document.querySelector('.hamburger-btn');
   const nav = document.querySelector('.mobile-menu');
 
-  hamburger.addEventListener('click', () => (
-    nav.classList.toggle('open')
-  ));
+  hamburger.addEventListener(
+    'click',
+    () => (nav.classList.toggle('open'), hamburger.classList.toggle('active')),
+  );
 
-  nav.addEventListener('click', () => (
-    nav.classList.toggle('open')
-  ))
-}
+  nav.addEventListener(
+    'click',
+    () => (nav.classList.toggle('open'), hamburger.classList.toggle('active')),
+  );
+};
 
 display();
