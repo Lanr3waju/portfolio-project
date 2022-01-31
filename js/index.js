ed763ebaa98c9c02f82ea018cf5b4145bcdfff51
Abass <Abassolanrewaju111@gmail.com>
2022-01-31 17:46:47 +0100

    create js object for works

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+const multiPostStories = [
+  {
+    jpeg: './img/work.webp',
+    webp: './img/work.jpg',
+    title: 'Multi-Post Stories Gain+Glory',
+    tag1: 'Ruby on Rails',
+    tag2: 'CSS',
+    tag3: 'JavaScript',
+    tag4: 'HTML',
+  },
+];
+
+
+
+const workSection = document.querySelector('.work-section-grid');
+
+
+
 const display = () => {
   const hamburger = document.querySelector('.hamburger-btn');
   const nav = document.querySelector('.mobile-menu');
